dbdf9cee8b6e57a1db482fa67962359e1171802d
Damian Bugaj <damianbugajit@gmail.com>
2023-02-16 23:29:41 +0100

    get tags from data-tags attribute step is done in generateTags

--- js/script.js
+++ js/script.js
@@ -124,7 +124,7 @@ function generateTags(){
 
     /* get tags from data-tags attribute */
 
-   // const articleTags = article.getAttribute('data-tags');
+    const articleTags = article.getAttribute('data-tags');
     console.log(articleTags);
 
     /* split tags into array */
