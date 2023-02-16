69e91797da5ef880fe7bba097ccda77ef0636b7a
Damian Bugaj <damianbugajit@gmail.com>
2023-02-16 14:59:10 +0100

    remove class 'active' from all articles

--- js/script.js
+++ js/script.js
@@ -14,11 +14,23 @@ const titleClickHandler = function(event){
 
     /* add class 'active' to the clicked link */
 
+
+
     /* remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('.posts article.active');
+
+    for(let activeArticle of activeArticles){
+     activeArticle.classList.remove('active');
+    };
 
     /* get 'href' attribute from the clicked link */
 
+
+
     /* find the correct article using the selector (value of 'href' attribute) */
+
+
 
     /* add class 'active' to the correct article */
   }
