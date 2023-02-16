bc87b5290b18e29704b60e5d34812f3704e325e6
Damian Bugaj <damianbugajit@gmail.com>
2023-02-16 14:43:29 +0100

    add eventListener to titleClickHandler

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const titleClickHandler = function(){
+    console.log('Link was clicked!');
+  }
+
+  const links = document.querySelectorAll('.titles a');
+
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
