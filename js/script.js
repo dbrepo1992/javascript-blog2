3afaa2d2f2e6500cef01f86f67aa30b07bf8fa42
Damian Bugaj <damianbugajit@gmail.com>
2023-02-16 15:11:40 +0100

    add eslint to package.json and this to clickedElement in script.js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
 'use strict';
 
 const titleClickHandler = function(event){
-    const clickedElement = this;
-    console.log('Link was clicked!');
+  const clickedElement = this;
+  console.log('Link was clicked!', event);
 
-    /* [DONE] remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
-    const activeLinks = document.querySelectorAll('.titles a.active');
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-    for(let activeLink of activeLinks){
-      activeLink.classList.remove('active');
-    }
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
 
 
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
   console.log('clickedElement:', clickedElement);
 
 
-    /* remove class 'active' from all articles */
+  /* remove class 'active' from all articles */
 
-    const activeArticles = document.querySelectorAll('.posts article.active');
+  const activeArticles = document.querySelectorAll('.posts article.active');
 
-    for(let activeArticle of activeArticles){
-     activeArticle.classList.remove('active');
-    };
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
-/* get 'href' attribute from the clicked link */
+  /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
-}
+};
 
 const links = document.querySelectorAll('.titles a');
 
