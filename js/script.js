9ed624fa78b904ed39941cad42021768e99833e3
Damian Bugaj <damianbugajit@gmail.com>
2023-02-16 14:45:13 +0100

    add instruction step by step in comments for titleClickHandler

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 'use strict';
 
-const titleClickHandler = function(){
+const titleClickHandler = function(event){
     console.log('Link was clicked!');
+
+    /* remove class 'active' from all article links  */
+
+    /* add class 'active' to the clicked link */
+
+    /* remove class 'active' from all articles */
+
+    /* get 'href' attribute from the clicked link */
+
+    /* find the correct article using the selector (value of 'href' attribute) */
+
+    /* add class 'active' to the correct article */
   }
 
   const links = document.querySelectorAll('.titles a');
 
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
-  }
