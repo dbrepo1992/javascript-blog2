3d6de86ed258f611a0f1e77e27f0abd815ff8208
Damian Bugaj <damianbugajit@gmail.com>
2023-02-16 17:34:02 +0100

    add generateTitleLinks and algorythm in comments

--- js/script.js
+++ js/script.js
@@ -48,3 +48,24 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+  /* for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
+
