eaa0849e24db87346706c295675a30ddd2db04a0
Damian Bugaj <damianbugajit@gmail.com>
2023-02-16 15:05:15 +0100

    update to step by step instruction in comments

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 'use strict';
 
 const titleClickHandler = function(event){
-    console.log('Link was clicked!', event);
+    const clickedElement = this;
+    console.log('Link was clicked!');
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
-     activeLink.classList.remove('active');
-    };
+      activeLink.classList.remove('active');
+    }
 
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
 
+  console.log('clickedElement:', clickedElement);
 
 
     /* remove class 'active' from all articles */
@@ -24,19 +26,15 @@ const titleClickHandler = function(event){
      activeArticle.classList.remove('active');
     };
 
-    /* get 'href' attribute from the clicked link */
+/* get 'href' attribute from the clicked link */
 
+  /* find the correct article using the selector (value of 'href' attribute) */
 
+  /* add class 'active' to the correct article */
+}
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+const links = document.querySelectorAll('.titles a');
 
-
-
-    /* add class 'active' to the correct article */
-  }
-
-  const links = document.querySelectorAll('.titles a');
-
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
