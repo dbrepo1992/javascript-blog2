443e96a2b92d9063390f13a3d96d6640a76102ee
Damian Bugaj <damianbugajit@gmail.com>
2023-02-17 16:18:19 +0100

    add constant href and read the attribute href of the clicked element in tagClickHandler

--- js/script.js
+++ js/script.js
@@ -170,7 +170,7 @@ function tagClickHandler(event){
 
   /* make a new constant "href" and read the attribute "href" of the clicked element */
 
-  //const href = clickedElement.querySelector('href');
+  const href = clickedElement.querySelector('href');
 
 
   /* make a new constant "tag" and extract tag from the "href" constant */
