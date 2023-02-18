02debc5d62929cf3ba8587eb881edbb4bc2a989e
Damian Bugaj <damianbugajit@gmail.com>
2023-02-18 19:46:38 +0100

    added function authorClickHandler and const optArticleAuthorSelector

--- js/script.js
+++ js/script.js
@@ -49,7 +49,8 @@ const titleClickHandler = function(event){
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
-  optArticleTagsSelector = '.post-tags .list';
+  optArticleTagsSelector = '.post-tags .list',
+  optArticleAuthorSelector = '.post .post-author';
 
 function generateTitleLinks(customSelector = ''){
 
@@ -235,4 +236,6 @@ function addClickListenersToAuthors(){
 }
 addClickListenersToAuthors();
 
+function authorClickHandler(){
 
+}
